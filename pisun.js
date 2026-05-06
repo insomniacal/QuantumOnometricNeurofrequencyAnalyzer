67c542b7cf9aa7624ce32ae647fc0aaa1aac21c2
insomnia <insomniacal6@gmail.com>
2026-05-06 20:35:58 +0200

    ебать пушка секс короч ссс + дс

--- pisun.js
+++ pisun.js
@@ -1,3 +1,5 @@
-let pipiska="оыоыоыо";
+const knopka = document.getElementById("knopka");
 
-
+knopka.addEventListener("click", ()  => {
+    knopka.textContent = "аххахахахах твоя мать сдохнет от рака";
+});
