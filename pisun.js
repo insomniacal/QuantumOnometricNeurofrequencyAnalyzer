9b73e6a3d9810e0c8374025fa934666a6ccfe491
dzigocu <rybnikovmark795@gmail.com>
2026-05-07 17:39:20 +0300

    ya v rot ebal

--- pisun.js
+++ pisun.js
@@ -3,3 +3,13 @@ const knopka = document.getElementById("knopka");
 knopka.addEventListener("click", ()  => {
     knopka.textContent = "аххахахахах твоя мать сдохнет от рака";
 });
+const list = document.querySelector('.list')
+const element = document.querySelector('.hui');
+
+const questionnaires = [
+    //1 test 
+    list.addEventListener('click', (el) => {
+        startQeestionnaire(questionnaires[0])})
+]
+//functions
+function startQUestionnaire(questionnaire){}
